43cc734fe8398158b3384b9447c8b8271f68cea0
brandon <brandonchoy05@gmail.com>
2024-02-05 16:20:24 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,33 @@
+num1 = int(input("Enter your frist number: "))
+operator = input("Enter in any math operator: ")
+num2 = int(input("Enter in  your second number: "))
 
+if operator == "+":
+	total = num1 + num2
+	print(f"{num1} + {num2} = {total}")
+elif operator == "-":
+	total = num1 - num2
+	print(f"{num1} - {num2} = {total}")
+elif operator == "*":
+	total = num1 * num2
+	print(f"{num1} * {num2} = {total}")
+elif operator == ("/"):
+	total = num1 / num2
+	print(f"{num1} / {num2} = {total}")
+elif operator == "//":
+	total = num1 // num2
+	print(f"{num1} // {num2} = {total}")
+elif operator == "/+":
+	total = num1 /+ num2
+	print(f"{num1} /+ {num2} = {total}")
+elif operator == "/-":
+	total = num1 /- num2
+	print(f"{num1} /- {num2} = {total}")
+elif operator == "**":
+	total = num1 ** num2
+	print(f"{num1} ** {num2} = {total}")
+elif operator == "%":
+	total = num1 % num2
+	print(f"{num1} % {num2} = {total}")
+else:
+	print("Try again")
